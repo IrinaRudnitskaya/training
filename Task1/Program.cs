c174866ce63c5331a08a54e968b4cdccb3e108e9
IrinaRudnitskaya <irina.m.rudnitsk@gmail.com>
2021-07-05 15:58:04 +0300

    middle commit

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter three-digit number ");
+            do
+            {
+
+            }
+
+            int sum = 0;
+            int n;
+            n = Int32.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine("sum of numbers " + sum);
+        }
+    }
+}
