69c39c96d93384f7ef1322c592d360d2bb404de2
IrinaRudnitskaya <irina.m.rudnitsk@gmail.com>
2021-07-05 17:14:14 +0300

    version1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; ConsoleKey
 
 namespace Task1
 {
@@ -6,15 +6,26 @@ namespace Task1
     {
         static void Main()
         {
+            void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+            {
+
+                if (Char.IsDigit(e.KeyChar)) return;
+                else
+                    e.Handled = true;
+            }
+            int sum = 0;
+            int n;
             Console.WriteLine("Enter three-digit number ");
             do
             {
+                n = Int32.Parse(Console.ReadLine());
+                if (n.ToString().Length != 3)
+                {
+                    Console.WriteLine("Error. Enter exactly three-digit number ");
+                }
+            } while (n.ToString().Length != 3);
 
-            }
-
-            int sum = 0;
-            int n;
-            n = Int32.Parse(Console.ReadLine());
+           
             while (n != 0)
             {
                 sum += n % 10;
